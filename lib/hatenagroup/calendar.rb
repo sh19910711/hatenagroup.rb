3b774bc7c11fdec5d10f093fd873b987b8f5bafa
Hiroyuki Sano <sh19910711@gmail.com>
2016-04-24 15:15:26 +0900

    fix(calendar): set logger

--- lib/hatenagroup/calendar.rb
+++ lib/hatenagroup/calendar.rb
@@ -5,17 +5,14 @@ module HatenaGroup
     def initialize(group, hatena_user_id, hatena_password)
       @group = group
       @agent = Mechanize.new
-      agent.user_agent  = USER_AGENT
-      agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      agent.log = Logger.new(STDERR)
+      agent.log.level = Logger::INFO
+      agent.user_agent = USER_AGENT
       login! hatena_user_id, hatena_password
     end
 
     def day(d)
       keyword d.strftime("%Y-%m-%d")
-    end
-
-    def keyword(s)
-      Keyword.new agent, group, s
     end
 
     private
@@ -26,11 +23,16 @@ module HatenaGroup
       HATENA_LOGIN_URL = 'https://www.hatena.ne.jp/login'
 
       def login!(hatena_user_id, hatena_password)
-        agent.get(HATENA_LOGIN_URL).form_with do |form|
-          form['name']     = hatena_user_id
-          form['password'] = hatena_password
+        agent.get(HATENA_LOGIN_URL).form_with(:action => '/login') do |form|
+          form['name']       = hatena_user_id
+          form['password']   = hatena_password
+          form['persistent'] = 0
           form.submit
         end
+      end
+
+      def keyword(s)
+        Keyword.new agent, group, s
       end
 
       class Keyword
