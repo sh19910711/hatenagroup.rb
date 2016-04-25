681dab4c1891d056008160b6d44c857431b4773d
Hiroyuki Sano <sh19910711@gmail.com>
2016-04-25 20:44:58 +0900

    add(spec/calendar): add test for Calendar#day(String)

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -3,12 +3,24 @@ require 'spec_helper'
 vcr_opts = { :cassette_name => 'calendar' }
 describe HatenaGroup::Calendar, :vcr => vcr_opts do
   describe :day do
-    context :update_content do
-      before { day.body = input }
-      it { expect(day.body).to include input }
-      let(:input) { "this-is-test" }
+    context 'day(Date)' do
+      context :update_content do
+        before { day.body = input }
+        it { expect(day.body).to include input }
+      end
+      let(:day) { calendar.day(Date.today) }
     end
-    let(:day) { calendar.day(Date.new(2016, 4, 1)) }
+
+    context 'day(String)' do
+      context :update_content do
+        before { day.body = input }
+        it { expect(day.body).to include input }
+        let(:input) { "this-is-test" }
+      end
+      let(:day) { calendar.day(Date.today.prev_month.strftime('%Y-%m-%d')) }
+    end
+
+    let(:input) { "this-is-test-#{Date.today}" }
   end
 
   include SpecHelper::HatenaEnv
